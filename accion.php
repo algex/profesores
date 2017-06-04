da79b4f68e1a40495e4a310b3adf6a0cde4b0e8a
root <root@DESKTOP-1JKQSHA.localdomain>
2017-06-04 11:56:52 -0400

    mejorando identacion

--- accion.php
+++ accion.php
@@ -1,22 +1,22 @@
 <?php
 // Declaración de Variables
-$Nombre=$_POST['Nombre'];
-$Cedula=$_POST['Cedula'];
-$Nhoras=$_POST['CantidadH'];
-$PrecioH=$_POST['CostoH'];
+$Nombre= $_POST['Nombre'];
+$Cedula= $_POST['Cedula'];
+$Nhoras= $_POST['CantidadH'];
+$PrecioH= $_POST['CostoH'];
 //Si algun campo esta vacio retornamos al formulario
-if ($Nhoras==0||$PrecioH==0){
+if ($Nhoras == 0 || $PrecioH == 0){
 	header('location:index.html');
 }
-if($Nombre==""||$Cedula==""){
+if ($Nombre == "" || $Cedula == ""){
 	header('location:index.html');
 }
 //operación
-$Totaln=$PrecioH*$Nhoras*4;
+$Totaln= $PrecioH * $Nhoras * 4;
 
-$Porcentaje=11*$Totaln/100;
+$Porcentaje= 11 * $Totaln / 100;
 
-$Total=$Totaln-$Porcentaje;
+$Total = $Totaln - $Porcentaje;
 ?>
 <!DOCTYPE html>
 <html>
@@ -29,19 +29,19 @@ $Total=$Totaln-$Porcentaje;
 
 	<div id="space">
 		<h3>Nombre del Profesor</h3>
-		<h4><?php echo$Nombre ?></h4>
+		<h4><?php echo $Nombre ?></h4>
 		<h3>Número de Cédula</h3>
-		<h4><?php echo$Cedula ?></h4>
+		<h4><?php echo $Cedula ?></h4>
 		<h3>Número de Horas trabajadas</h3>
-		<h4><?php echo$Nhoras ?></h4>
+		<h4><?php echo $Nhoras ?></h4>
 		<h3>Precio de cada hora</h3>
-		<h4><?php echo$PrecioH ?></h4>
+		<h4><?php echo $PrecioH ?></h4>
 		<h3>Sueldo Base</h3>
-		<h4><?php echo$Totaln ?></h4>
+		<h4><?php echo $Totaln ?></h4>
 		<h3>Descuentos Realizados (11%)</h3>
-		<h4><?php echo$Porcentaje ?></h4>
+		<h4><?php echo $Porcentaje ?></h4>
 		<h3>Total a cobrar</h3>
-		<h4><?php echo$Total ?></h4>
+		<h4><?php echo $Total ?></h4>
 		<a href="index.html" id="return">Realizar otra consulta</a>
 	</div>
 	<h2 id="make">Realizado por: Gerardo Oropeza CI: 26.195.902</h2>
